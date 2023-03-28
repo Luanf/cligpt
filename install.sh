6f1228fc86ba08c6d714aa8b13874857647d3169
Mehmet Y. Alici <mehmet.y.alici@outlook.com>
2023-03-28 20:09:55 +0200

    add bash install support

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Append the cligpt definition to the user's .zshrc file
-cat cligpt.zsh >> ~/.zshrc
+if [[ -n "$BASH_VERSION" ]]; then
+  cat cligpt.zsh >> ~/.bashrc
+elif [ -n "$ZSH_VERSION" ]; then
+  cat cligpt.zsh >> ~/.zshrc
+else
+  echo "Install unsuccessful. Shell is not supported."
+  return 1
+fi
 
 echo "Installed. Reload your shell to use cligpt, and make sure you have the OPENAI_API_KEY environment variable set."
